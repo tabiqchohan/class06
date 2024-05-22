6e439b311cf36ab9d0e67ae0bc4ee5f8afa37abc
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-05-22 10:15:35 -0700

    Add files via upload

---
+++ module.js
@@ -0,0 +1,5 @@
+export function fuullName() {
+    const name1 = "ubaid";
+    const name2 = "ur rehman";
+    return name1 + name2;
+}
---
+++ module.ts
@@ -0,0 +1,5 @@
+export function fuullName(){
+    const name1:string = "ubaid"
+    const name2:string = "ur rehman"
+    return name1 + name2
+}
